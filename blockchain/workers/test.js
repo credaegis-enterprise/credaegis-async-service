84940b60954d67c1c9f430c4bf0d0602cb8b179a
AllenBastian <reachallenbastian@gmail.com>
2024-12-20 23:54:01 +0530

    fix: working with producer inside a consumer

--- blockchain/workers/test.js
+++ blockchain/workers/test.js
@@ -6,9 +6,10 @@ const test = async(approvalRequestChannel,approvalResponseChannel,errorChannel)=
     console.log("Listening for test requests");
     try {
     
-        approvalRequestChannel.bindQueue('APPROVAL_REQUEST_QUEUE','DIRECT_EXCHANGE','approval_request');
+        
         approvalRequestChannel.prefetch(1);
         approvalRequestChannel.consume('APPROVAL_REQUEST_QUEUE',async (message) => {
+            console.log("askjskajsjaksjkajskj")
             try{
             const content = JSON.parse(message.content.toString());
             console.log("content",content);
@@ -17,13 +18,15 @@ const test = async(approvalRequestChannel,approvalResponseChannel,errorChannel)=
             const updatedResult = {
                 approvalId: content.approvalId,
                 hash: result[0].Hash,
-                stored: result[0].Stored
+                stored: result[0].Stored,
+                userId: content.userId
             }
 
             console.log("updatedResult",updatedResult);
-            
+
             approvalResponseChannel.publish('DIRECT_EXCHANGE','approval_response',Buffer.from(JSON.stringify(updatedResult)));
             approvalRequestChannel.ack(message);
+            
         }
         catch(error){
                 approvalRequestChannel.nack(message,false,false);
